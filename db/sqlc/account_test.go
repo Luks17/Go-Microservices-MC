8d0bf364ec4a16a1b307a1d6129cfd5c47c99e5b
Luks17 <lucasthidavort@gmail.com>
2024-09-24 10:32:46 -0400

    feat(sqlc): create account test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,5 +1,28 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
 
-func TestCreateAccount(t *testing.T) {}
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "Nestor",
+		Balance:  "100.50",
+		Currency: CurrenciesUSD,
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+}
